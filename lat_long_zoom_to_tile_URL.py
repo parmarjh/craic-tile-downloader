caba9ffba3640982acec1fb1399a4536407553e4
Rob Jones <jones@craic.com>
2017-11-16 10:02:57 -0800

    add parentheses to print statement to make them compatible with python 2 and 3

--- lat_long_zoom_to_tile_URL.py
+++ lat_long_zoom_to_tile_URL.py
@@ -15,18 +15,18 @@ def lat_long_zoom_to_pixel_coords(lat, lon, zoom):
     p = Point()
     sinLat = math.sin(lat * math.pi/180.0)
     x = ((lon + 180) / 360) * 256 * math.pow(2,zoom)
-    y = (0.5 - math.log((1 + sinLat) / (1 - sinLat)) / 
+    y = (0.5 - math.log((1 + sinLat) / (1 - sinLat)) /
           (4 * math.pi)) * 256 * math.pow(2,zoom)
     p.x = int(math.floor(x))
     p.y = int(math.floor(y))
-    print "\nThe pixel coordinates are x = {} and y = {}".format(p.x, p.y)
+    print("\nThe pixel coordinates are x = {} and y = {}".format(p.x, p.y))
     return p
 
 def pixel_coords_to_tile_address(x,y):
     t = Tile()
     t.x = int(math.floor(x / 256))
     t.y = int(math.floor(y / 256))
-    print"\nThe tile coordinates are x = {} and y = {}".format(t.x, t.y)
+    print("\nThe tile coordinates are x = {} and y = {}".format(t.x, t.y))
     return t
 
 def tile_coords_and_zoom_to_quadKey(x, y, zoom):
@@ -39,7 +39,7 @@ def tile_coords_and_zoom_to_quadKey(x, y, zoom):
         if(y & mask) != 0:
             digit += 2
         quadKey += str(digit)
-    print "\nThe quadkey is {}".format(quadKey)
+    print("\nThe quadkey is {}".format(quadKey))
     return quadKey
 
 def quadKey_to_url(quadKey):
@@ -47,13 +47,13 @@ def quadKey_to_url(quadKey):
         f = open('cfg/api_key')
         api_key = f.read()
     except:
-        print ("Something is wrong with your API key.\n"
+        print("Something is wrong with your API key.\n"
                "Do you even have an API key?")
 
-    #TODO get this into a config file, and set up others (Google, OSM, etc) 
+    #TODO get this into a config file, and set up others (Google, OSM, etc)
     tile_url = ("http://t0.tiles.virtualearth.net/tiles/a{}.jpeg?"
                 "g=854&mkt=en-US&token={}".format(quadKey, api_key))
-    print "\nThe tile URL is: {}".format(tile_url)
+    print("\nThe tile URL is: {}".format(tile_url))
     return tile_url
 
 
@@ -69,14 +69,14 @@ def lat_long_zoom_to_URL(lat, lon, zoom):
     quadKey = tile_coords_and_zoom_to_quadKey(tile.x, tile.y, zoom)
     tile_url = quadKey_to_url(quadKey)
     return tile_url
-   
+
 if __name__ == "__main__":
 
     # This script is intended to provide functions to another script.
 
     # The __main__ function here is for testing and convenience;
     # it accepts a latitude, longitude, and zoom level as arguments and
-    # calls the functions that translate those inputs into a tile URL, which 
+    # calls the functions that translate those inputs into a tile URL, which
     # is printed to the console along with the pixel and tile coordinates.
 
     lat = float(sys.argv[1])
